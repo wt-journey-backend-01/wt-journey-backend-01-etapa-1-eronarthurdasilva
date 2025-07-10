1bbf4ca780114f1164b5230d397f8ba0c7040aae
Eron Silva <er0narthur23@gmail.com>
2025-07-10 20:52:35 -0300

    Criacao inicial do projeto - Criei estruturas das pastas conforme o desafio - inicializei o projeto com npm init -y e instalei o express - Criei o arquivo server.js e adicionei o código inicial para rodar o servidor

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const path = require('path');
+const fs = require('fs');
+
+//
+
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor da DevBurger rodando em localhost:${PORT}`);
+});
